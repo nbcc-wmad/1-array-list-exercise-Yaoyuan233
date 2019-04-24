383aaf028077ee4d0d3535edf1242915e7a36f4e
Yao <43917704+Yaoyuan233@users.noreply.github.com>
2019-04-24 14:26:30 -0300

    ?

--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,53 @@ namespace ArrayListSimpleEx
 {
     public partial class Form1 : Form
     {
+
+        ArrayList exercise = new ArrayList();
+
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = string.Empty;
+            for(int i = 0; i < exercise.Count; i++)
+            {
+                lblMessage.Text = lblMessage.Text + exercise[i].ToString()+" ";
+            }
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            exercise.Reverse();
+            lblMessage.Text = string.Empty;
+            for (int i = 0; i < exercise.Count; i++)
+            {
+                lblMessage.Text = lblMessage.Text + exercise[i].ToString() + " ";
+            }
+            exercise.Reverse();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            exercise.Insert(1,txtSecondPos.Text.Trim());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            exercise.RemoveAt(1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            exercise.Add("I");
+            exercise.Add("Love");
+            exercise.Add("Programming");
+            exercise.Add("So");
+            exercise.Add("Much");
         }
     }
 }
